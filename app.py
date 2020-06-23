320a7d08f1ccb1cd4a75e44f0e4e31424e98ea02
Omenranr <omar.rachidilalaoui@um5s.net.ma>
2020-06-23 15:38:06 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import pandas as pd 
+# from fbprophet import Prophet
+
+app = Flask(__name__) 
+
+@app.route("/") 
+def home_view():
+	return {"timeline":[1, 2, 3]}
+
+
+app.run(port="9999")
